a51570cf8924747b348c071cd4c944d86b907c1a
Kerolos-Morcos <122631134+Kerolos-Morcos@users.noreply.github.com>
2024-10-10 20:14:09 +0300

    title and subtitle widgets

--- lib/features/home/presentation/views/widgets/book_details_view_body.dart
+++ lib/features/home/presentation/views/widgets/book_details_view_body.dart
@@ -1,3 +1,5 @@
+import 'package:bookly_app/constants.dart';
+import 'package:bookly_app/core/utils/styles.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/custom_book_details_app_bar.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/featured_list_view_item.dart';
 import 'package:flutter/material.dart';
@@ -11,11 +13,31 @@ class BookDetailsViewBody extends StatelessWidget {
     return ListView(
       children: [
         const CustomBookDetailsAppBar(),
-        const SizedBox(height: 30),
+        const SizedBox(height: 25),
         Padding(
-          padding: EdgeInsets.symmetric(horizontal: width * 0.20),
+          padding: EdgeInsets.symmetric(horizontal: width * 0.2),
           child: const CustomBookImage(),
         ),
+        const SizedBox(height: 30),
+        Text(
+          'The Jungle Book',
+          style: Styles.titleLargeBold22.copyWith(
+            fontSize: 30,
+            fontFamily: kGTSectraFine,
+            fontWeight: FontWeight.bold,
+          ),
+          textAlign: TextAlign.center,
+        ),
+        const SizedBox(height: 2),
+        Text(
+          'Rudyard Kipling',
+          style: Styles.subTitleMedium18.copyWith(
+            color: Colors.grey,
+            fontStyle: FontStyle.italic,
+          ),
+          textAlign: TextAlign.center,
+        ),
+        const SizedBox(height: 8),
       ],
     );
   }
